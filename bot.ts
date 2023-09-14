a6daf07401933ae1123f90aed639a7ee006242da
Viachaslau Rouski <vrovsky@gmail.com>
2023-09-14 16:12:52 +0100

    bot url update

--- bot.ts
+++ bot.ts
@@ -25,7 +25,7 @@ bot.onText(/\/start/, (msg: any) => { //todo rm any
         reply_markup: {
             inline_keyboard:
                 [
-                    [{ text: '🏦 Launch App', web_app: { url: 'https://app.evaa.finance' } }],
+                    [{ text: '🏦 Launch App', web_app: { url: 'https://app.evaa.finance/#/twa' } }],
                     [{ text: '📊 Market Data', web_app: { url: 'https://app.evaa.finance/#/market' } }, { text: '💰 Get Testnet Tokens', callback_data: 'Get tokens' }]
                 ]
         },
